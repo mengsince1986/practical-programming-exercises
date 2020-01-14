65ab8c71d104926950f4cc5947f3060d850aacd1
mengsince1986 <mengcode1986@gmail.com>
2020-01-14 13:42:41 +1300

    update exercise c09 c05

--- c09_exercise_05.py
+++ c09_exercise_05.py
@@ -5,6 +5,16 @@ understand what the function does and how, and then compare your solution with
 those of at least two other people. How similar are they? Why do they differ?
 """
 def mystery_function(values):
+    """
+    (nested list) -> nested list
+
+    Return a copy of the list, values, and the sublists it contains.
+    The top-level sub-lists have their elements reversed in the returned list.
+
+    >>> mystery_function([[1, 2, 3, 4 , 5], ['a', 'b', 'c', 'd', 'e']])
+    [[5, 4, 3, 2, 1], ['e', 'd', 'c', 'b', 'a']]
+    """
+
     result = []
     for sublist in values:
         result.append([sublist[0]])
