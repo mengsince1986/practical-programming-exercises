d1e3f357ffd344d6f4cf662f4800a35b1b77eeb3
mz <mengcode1986@gmail.com>
2020-04-12 17:03:28 +1200

    add observations_to_birds_list and count_birds

---
+++ observations_to_birds_list.py
@@ -0,0 +1,30 @@
+bird_to_observations = {'canada goose': 5, 'northern fulmar': 1,
+                        'long-tailed jaeger': 2, 'snow goose': 1}
+
+# Invert the dictionary
+
+observations_to_birds_list = {}
+
+for bird, observations in bird_to_observations.items():
+    if observations in observations_to_birds_list:
+        observations_to_birds_list[observations].append(bird)
+    else:
+        observations_to_birds_list[observations] = [bird]
+
+print(observations_to_birds_list)
+# {1: ['northern fulmar', 'snow goose'], 2: ['long-tailed jaeger'],
+#  5: ['canada goose']}
+
+# Print the inverted dictionary
+observations_sorted = sorted(observations_to_birds_list.keys())
+for observations in observations_sorted:
+    print(observations, ':', end=" ")
+    for bird in observations_to_birds_list[observations]:
+        print(' ', bird, end=" ")
+    print()
+
+"""
+1 : northern fulmar snow goose
+2 : long-tailed jaeger
+5 : canada goose
+"""
